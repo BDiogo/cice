5aeea4f5e7783e0195d7aa7afbb50a81fa48425f
Beatriz José Monteiro Diogo <beanes.diogo@gmail.com>
2016-12-19 09:26:26 +0000

    pagina workshops

---
+++ Redesign/workshop.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+	<meta charset="UTF-8">
+	<title>CICE</title>
+	<meta name="description" content="CICE">
+	<meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="apple-mobile-web-app-capable" content="yes">
+	<meta name="apple-mobile-web-app-status-bar-style" content="black">
+	<meta name="mobile-web-app-capable" content="yes">
+	<meta name="author" content="Arquitecturas Virtuais ©2016">
+	<!--Styles!-->
+	<!--[if lt IE 9]>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.min.js"></script>
+	<![endif]-->
+	<!--[if (gte IE 9) | (!IE)]><!-->
+	<script type="text/javascript" charset="UTF-8" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<!--<![endif]-->
+	<link rel="stylesheet" type="text/css" href="CSS/cssreset.css">
+	<link rel="stylesheet" type="text/css" href="CSS/style.css">
+</head>
+
+<body>
+	<!--CONTENT-->
+	<header class="header">
+		<div id="js-hamb" class="hamb">
+			<div class="hamb__element hamb--black"></div>
+			<div class="hamb__element hamb--black"></div>
+			<div class="hamb__element hamb--black"></div>
+		</div>
+	</header>
+	<div class="menu--full menu--hidden">
+		<div class="menu--full__logo"></div>
+		<ul>
+			<li><a href="index.html#js-first-wrapper">Sobre</a><li>
+			<li><a href="index.html#js-second-wrapper">Programação</a><li>
+			<li><a href="index.html#js-third-wrapper">Workshops</a><li>
+			<li><a href="index.html#js-fourd-wrapper">Exposição</a><li>
+			<li><a href="index.html#">Entrevista</a><li>
+		</div>
+		<main class="main__content">
+			<aside class="hero__side__text side__text--left text--black"><span class="underline">DArq</span> <span class="symbol">►</span> UCoimbra</aside>
+			<aside class="hero__side__text side__text--right text--black"><span class="underline">20 — 30 Abril 2016</span></aside>
+
+
+				<section id="js-first-wrapper" class="wrapper">
+					<div class="wrapper__content">
+						<div class="wrapper__content__articles">
+							<article class="content__article__open">
+								<h2 class="content__subtitle">Segunda Feira — 20 de Abril</h2>
+								<h1 class="content__subtitle">Workshop de Rhino & C4D</h1>
+								<div class="article__image__open"></div>
+								
+								<p>O desenho é a ferramenta de trabalho de eleição para um arquiteto. Começando por um primeiro esquisso para “escrever” uma ideia, esboçar um projeto. Quando é preciso passar a ideia para algo concreto, as formas de o fazer têm vindo a evoluir. No passado e até à bem poucos anos, os projetos eram todos desenvolvidos de forma manual. Com a evolução dos computadores e dos programas de desenho, a arquitectura passou a contar com uma ferramenta de trabalho que veio facilitar e ajudar no desenvolvimento de novas formas no projeto. A evolução da sociedade e a constante necessidade de apreensão das formas de uma maneira mais rápida e eficaz, fez com que o trabalho do arquitecto tivesse que acompanhar esta evolução Assim, o modelo 3D torna-se uma ferramenta extremamente importante para o estudo e desenvolvimento do próprio projeto, desde o estudo das formas, dos materiais a utilizar, o estudo dos alçados, do próprio volume de construção e a envolvência com o meio exterior. As imagens 3D e animações realísticas, são por isso uma ferramenta complementar ao projeto de arquitetura, sendo que, quanto mais realísticas forem as imagens, mais convincente o projeto..</p>
+							</article>
+						</div>
+					</div>
+				</section>
+
+			
+				<footer class="footer">
+					<div class="wrapper__content">
+						<h1>+(351) 239 857 019<br>colegiodasartes@colegiodasartes.uc.pt<br>www.uc.pt/colegioartes</h1>
+						<h1>Colégio das Artes<br>Apartado 3066<br>3001-401 Coimbra</h1>
+					</div>
+				</footer>
+			</main>
+			<!--<section class="other contain"></section>-->
+			<script type='text/javascript' charset="UTF-8" src='JS/app_workshop.js'></script>
+			<script type='text/javascript' charset="UTF-8" src="JS/jquery.easing.js"></script>
+		</body>
+		</html>
